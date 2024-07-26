ecd90c9a374904012c1b9a9ac16e93fe02e2a26a
Nayid04 <nayid2004@gmail.com>
2024-07-26 11:36:07 -0500

    Agregacion de variables de entorno

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+    //apiUrlBase: 'https://drinkobackend.azurewebsites.net',
+    //apiUrlBase: 'https://localhost:7248',
+    apiUrlBase: 'https://localhost:5204',
+    production: true
+};
