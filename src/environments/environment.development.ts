423e425f03f16c6b0b08040a03f29e3db7335ca1
Nayid04 <nayid2004@gmail.com>
2024-07-26 21:54:36 -0500

    Cambio en las variables de entorno

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
     apiUrlBase: 'https://backendsac.azurewebsites.net/',
     //apiUrlBase: 'https://localhost:7248',
     //apiUrlBase: 'https://localhost:5204',
-    production: false
+    production: true
 };
